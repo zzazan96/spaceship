bc83ba12ba5c32ba2748a8ce83037780f18742b2
zzazan96 <zzazan96@naver.com>
2022-07-25 19:43:53 +0900

    questions

--- TitanicPj/TitanicApp/views.py
+++ TitanicPj/TitanicApp/views.py
@@ -3,5 +3,44 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
-    return render(request, 'TitanicApp/index.html')
+def Main(request):
+    return render(request, 'TitanicApp/Main.html')
+
+def Questions1(request):
+    return render(request, 'TitanicApp/Questions1.html')
+
+def Questions2(request):
+    return render(request, 'TitanicApp/Questions2.html')
+
+def Questions3(request):
+    return render(request, 'TitanicApp/Questions3.html')
+
+def Questions4(request):
+    return render(request, 'TitanicApp/Questions4.html')
+
+def Questions5(request):
+    return render(request, 'TitanicApp/Questions5.html')
+
+def Questions6(request):
+    return render(request, 'TitanicApp/Questions6.html')
+
+def Questions7(request):
+    return render(request, 'TitanicApp/Questions7.html')
+
+def Questions8(request):
+    return render(request, 'TitanicApp/Questions8.html')
+
+def Questions9(request):
+    return render(request, 'TitanicApp/Questions9.html')
+
+def Questions10(request):
+    return render(request, 'TitanicApp/Questions10.html')
+
+def Questions11(request):
+    return render(request, 'TitanicApp/Questions11.html')
+
+def Questions12(request):
+    return render(request, 'TitanicApp/Questions12.html')
+
+def Questions13(request):
+    return render(request, 'TitanicApp/Questions13.html')
--- TitanicPj/TitanicPj/urls.py
+++ TitanicPj/TitanicPj/urls.py
@@ -20,5 +20,20 @@ import TitanicApp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TitanicApp.views.index, name='index'),
+    path('', TitanicApp.views.Main, name='Main'),
+    path('Main/', TitanicApp.views.Questions1, name='Questions1'),
+    path('Main/Questions1/', TitanicApp.views.Questions2, name='Questions2'),
+    path('Main/Questions1/Questions2', TitanicApp.views.Questions3, name='Questions3'),
+    path('Main/Questions1/Questions2/Questions3', TitanicApp.views.Questions4, name='Questions4'),
+    path('', TitanicApp.views.Questions5, name='Questions5'),
+    path('', TitanicApp.views.Questions6, name='Questions6'),
+    path('', TitanicApp.views.Questions7, name='Questions7'),
+    path('', TitanicApp.views.Questions8, name='Questions8'),
+    path('', TitanicApp.views.Questions9, name='Questions9'),
+    path('', TitanicApp.views.Questions10, name='Questions10'),
+    path('', TitanicApp.views.Questions11, name='Questions11'),
+    path('', TitanicApp.views.Questions12, name='Questions12'),
+    path('', TitanicApp.views.Questions13, name='Questions13'),
+
+    
 ]
